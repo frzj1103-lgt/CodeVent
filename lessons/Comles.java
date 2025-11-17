fd74fd296dc4318c05e19008b8ab73f929d0078e
Yuu2-bit <factorbrent26@gmail.com>
2025-11-17 16:45:40 +1300

    first commit

---
+++ lessons/Comles.java
@@ -0,0 +1,53 @@
+public class Comles {
+    public static void main(String[] args) {
+        System.out.println("============================================================================================================================");
+        System.out.println("                                                 JAVA COMMENTS");
+        System.out.println("============================================================================================================================");
+        System.out.println("");
+        System.out.println("Comments can be used to explain Java code, and to make it more readable. ");
+        System.out.println("It can also be used to prevent execution when testing alternative code.");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Single-line Comments");
+        System.out.println("");
+        System.out.println("Single-line comments start with two forward slashes (//).");
+        System.out.println("Any text between // and the end of the line is ignored by Java (will not be executed).");
+        System.out.println("");
+        System.out.println("This example uses a single-line comment before a line of code:");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Example");
+        System.out.println("");
+        System.out.println("// This is a comment");
+        System.out.println("System.out.println(\"Hello World\");");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("This example uses a single-line comment at the end of a line of code:");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Example");
+        System.out.println("");
+        System.out.println("System.out.println(\"Hello World\"); // This is a comment");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Java Multi-line Comments");
+        System.out.println("");
+        System.out.println("Multi-line comments start with /* and ends with */.");
+        System.out.println("Any text between /* and */ will be ignored by Java.");
+          System.out.println("");
+        System.out.println("This example uses a multi-line comment (a comment block) to explain the code:");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Example");
+        System.out.println("");
+        System.out.println("/* The code below will print the words Hello World");
+        System.out.println("to the screen, and it is amazing */");
+        System.out.println("System.out.println(\"Hello World\");");
+        System.out.println("");
+        System.out.println("");
+       
+    }
+        
+    }
+    
+
